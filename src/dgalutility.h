02e0867369bc4417f0fffad4d3bfb9d774db98d8
Colin Steifel <csteifel@gmail.com>
2013-09-15 18:48:26 -0400

    Adding ability for debug logging system

---
+++ src/dgalutility.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+namespace dgal {
+
+#ifdef DEBUG
+	void log(std::string message){
+		std::cout << message << std::endl;
+	}
+#else
+	void log(std::string message){
+		(void) message;
+	}
+#endif
+
+}
