221211cdfbdd422d641e66e35b8663e13c4dcad1
Colin Steifel <csteifel@gmail.com>
2013-09-15 18:19:22 -0400

    Moving implementations to header. Due to template use now

--- src/population.h
+++ src/population.h
@@ -1,23 +1,31 @@
 #ifndef POPULATION_H
 #define POPULATION_H
 
+#include "individual.h"
 #include <vector>
 #include <utility>
-#include "individual.h"
 
 namespace dgal {
-	class population {
+	template <typename T> class population {
+		typedef std::shared_ptr<T> custIndPtr;
 		public:
 			void addOutsideBests(const std::vector<std::pair<std::string, double> >& outsiders);
 		protected:
 			//virtual to allow for custom population controller
 			virtual void nextGeneration();
-			virtual void generateNewIndividuals();
-			virtual bool checkGoals() const;
-			std::vector<dgal::individual> individuals;
+			//virtual void generateNewIndividuals();
+			//virtual bool checkGoals() const;
+			std::vector<custIndPtr> individuals;
 			//Individual buffer for holding bests from other nodes until used.
-			std::vector<dgal::individual> individualBuffer;
+			std::vector<custIndPtr > individualBuffer;
 	};
+
+
+	template <typename indType> void population<indType>::nextGeneration(){
+		//TODO: implementation
+		std::cout << "TEST" << std::endl;
+	}
+	
 }
 
 #endif
