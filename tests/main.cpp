39f25319a4bce5eb03a2bc765374f704b8ee06c5
Colin Steifel <csteifel@gmail.com>
2013-09-15 20:30:31 -0400

    Adding beginning of test for node manager

--- tests/main.cpp
+++ tests/main.cpp
@@ -10,9 +10,13 @@ class test : public dgal::individual {
 		test(const std::shared_ptr<test> a, const std::shared_ptr<test> b) : dgal::individual(a, b) {}
 		//std::string serialize() const {std::cout << "A" << std::endl; return "";}
 		void print(){ for(size_t i = 0; i < weights.size(); i++){ std::cout << weights[i] << " "; } std::cout << std::endl; }
+
+		void run(){ fitness = rand(); }
 };
 
 int main(){
+
+	//Testing custom individuals
 	std::srand(std::time(0));
 	std::shared_ptr<test> A(new test);
 	std::shared_ptr<test> B(new test);
@@ -27,6 +31,14 @@ int main(){
 	std::cout << "C:\n";
 	C->print();
 
+
+	//End testing of individuals
+	
+	
+
+	//Start testing of node manager
+	
+	dgal::nodeManager<test> node;
 
 	
 
