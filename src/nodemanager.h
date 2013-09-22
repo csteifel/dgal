bdf244efe7c47d0d38eced129b42b04d4e5cb519
Colin Steifel <csteifel@gmail.com>
2013-09-22 17:21:02 -0400

    Set up nodemanager constructor. Changed population to be dynamically allocated. Dynamically allocate population to control the start of generation execution.

--- src/nodemanager.h
+++ src/nodemanager.h
@@ -8,6 +8,7 @@
 #include "messenger.h"
 #include "population.h"
 #include "dgalutility.h"
+#include "unistd.h"
 
 
 namespace dgal {
@@ -15,9 +16,11 @@ namespace dgal {
 		public:
 			nodeManager();
 			void initialize();
+			void initiateMessaging();
 		private:
-			dgal::population<indType> pop;
+			dgal::population<indType> * pop;
 			std::unique_ptr<dgal::messenger> nodeMessenging;
+			bool stop = false;
 	};
 
 
@@ -30,6 +33,21 @@ namespace dgal {
 		//TODO: this is included because of temporary random generation scheme that should be replaced
 		dgal::log("Node manager started");
 		std::srand(std::time(0));
+
+		std::thread t(&nodeManager<indType>::initiateMessaging, this);
+		pop = new dgal::population<indType>;
+		stop = true;
+		t.join();
+	}
+
+	template <typename indType> void nodeManager<indType>::initiateMessaging(){
+		//TODO: implement
+
+		
+
+		if(stop){
+			return;
+		}
 	}
 
 }
