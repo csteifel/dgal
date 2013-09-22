ea7d9d6a54fe3a3ee5f5d994563fa3e302c5bc6e
Colin Steifel <csteifel@gmail.com>
2013-09-22 17:16:35 -0400

    Added start of generation control for populations

--- src/population.h
+++ src/population.h
@@ -5,7 +5,9 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <atomic>
 #include "dgalutility.h"
+#include <unistd.h>
 
 namespace dgal {
 	template <typename indType> class population {
@@ -15,57 +17,85 @@ namespace dgal {
 			void addOutsideBests(const std::vector<std::pair<std::string, double> >& outsiders);
 		protected:
 			//virtual to allow for custom population controller
-			virtual void nextGeneration();
-			virtual void generateNewIndividuals();
-			virtual void chooseParents();
+			void nextGeneration();
+			void generateNewIndividuals();
+			void chooseParents();
+			void run();
 			//virtual bool checkGoals() const;
 
 			std::vector<custIndPtr> individuals;
 			//Individual buffer for holding bests from other nodes until used.
 			std::vector<custIndPtr > individualBuffer;
+			std::atomic<bool> bufferDirty;
+
 			size_t numIndividuals = 10;
 			size_t generationNum = 0;
+			size_t maxGeneration = 10;
+
 	};
 
-
 	template <typename indType> population<indType>::population(){
-		nextGeneration();
+		bufferDirty.store(false);
+		if(generationNum == 0){
+			generateNewIndividuals();
+		}
+		run();
+	}
+
+	template <typename indType> void population<indType>::addOutsideBests(const std::vector<std::pair<std::string, double> >& outsiders){
+		for(size_t i = 0; i < outsiders.size(); i++){
+			custIndPtr recieved(new indType(outsiders[i].first, outsiders[i].second));
+			individualBuffer.push_back(std::move(recieved));
+		}
+		bufferDirty.store(true);
 	}
 
 	template <typename indType> void population<indType>::nextGeneration(){
-		if(individuals.size() != 0){
-			//Pick out bests if not just starting up
-			chooseParents();
-		}
+		//Pick out bests if not just starting up
+		chooseParents();
 
 		//Fill in the rest of the population with new individuals
 		generateNewIndividuals();
 
 
-		//Run this generation
-		dgal::log("Running generation " + std::to_string(generationNum));
-		for(size_t i = 0; i < individuals.size(); i++){
-			individuals[i]->run();
-		}
-
-		std::sort(individuals.begin(), individuals.end());
+		dgal::log("Sorting");
+		std::sort(individuals.begin(), individuals.end(),
+			[](const std::shared_ptr<dgal::individual> a, const std::shared_ptr<dgal::individual> b){
+				if(a->getFitness() <= b->getFitness()){ return false; } return true;
+			});
 
 
 		for(size_t i = 0; i < individuals.size(); i++){
 			dgal::log("Fitness " + std::to_string(individuals[i]->getFitness()));
 		}
-
-
-	}
-
-	template <typename indType> void population<indType>::chooseParents(){
-		//TODO: implement
+	
 	}
 
 	template <typename indType> void population<indType>::generateNewIndividuals(){
 		while(individuals.size() < numIndividuals){
 			dgal::log("Adding individual");
 			individuals.push_back(custIndPtr(new indType));
+		}
+	}
+
+	template <typename indType> void population<indType>::chooseParents(){
+		//TODO: implement
+	}
+
+	template <typename indType> void population<indType>::run(){
+		dgal::log("Running generation " + std::to_string(generationNum));
+
+		//Run this generation
+		//TODO: put in thread pool individual running
+		for(size_t i = 0; i < individuals.size(); i++){
+			individuals[i]->run();
+		}
+
+		++generationNum;
+
+		if(generationNum < maxGeneration){
+			nextGeneration();
+			run();
 		}
 	}
 }
