f9eddccdf9126aef15a7d5fc2cecf2a30a5013ec
Colin Steifel <csteifel@gmail.com>
2013-09-29 16:38:54 -0400

    Added second template param

--- src/nodemanager.h
+++ src/nodemanager.h
@@ -12,7 +12,7 @@
 
 
 namespace dgal {
-	template <typename indType> class nodeManager {
+	template <typename indType, typename messagingType> class nodeManager {
 		public:
 			nodeManager();
 			void initialize();
@@ -25,28 +25,32 @@ namespace dgal {
 
 
 
-	template<typename indType> nodeManager<indType>::nodeManager(){
+	template<typename indType, typename messagingType> nodeManager<indType, messagingType>::nodeManager(){
 		initialize();
 	}
 
-	template <typename indType> void nodeManager<indType>::initialize(){
+	template <typename indType, typename messagingType> void nodeManager<indType, messagingType>::initialize(){
 		//TODO: this is included because of temporary random generation scheme that should be replaced
 		dgal::log("Node manager started");
 		std::srand(std::time(0));
 
-		std::thread t(&nodeManager<indType>::initiateMessaging, this);
+		std::thread t(&nodeManager<indType, messagingType>::initiateMessaging, this);
 		pop = new dgal::population<indType>;
 		stop = true;
 		t.join();
 	}
 
-	template <typename indType> void nodeManager<indType>::initiateMessaging(){
+	template <typename indType, typename messagingType> void nodeManager<indType, messagingType>::initiateMessaging(){
 		//TODO: implement
-
-		
-
-		if(stop){
-			return;
+		messagingType messageController;
+		std::vector<std::pair<std::string, double> > bestContainer;
+		while(true){
+			messageController.getBests(bestContainer);
+			
+			bestContainer.clear();
+			if(stop){
+				return;
+			}
 		}
 	}
 
