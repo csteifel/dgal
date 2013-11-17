5067ba796bcf214608c10fc41c3d1a5ec47f93a2
Colin Steifel <colin@steifel.net>
2013-11-17 14:39:09 -0500

    Started handlers for connections and started to set up connection objects

--- coordinator/src/main.cpp
+++ coordinator/src/main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 #include <thread>
+#include <vector>
+#include <memory>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <errno.h>
 #include <string.h>
+#include <string>
+#include <unistd.h>
 
-bool listen(char *port){
+
+class dgalNode{
+	public:
+		dgalNode(const int socket, const std::string addr, const unsigned short port) : sockfd(socket), listenPort(port), addressInfo(addr){}
+		dgalNode(const dgalNode&) = delete;
+	private:
+		const int sockfd;
+		unsigned short listenPort;
+		std::string addressInfo;
+};
+
+
+void handleNewConnections(const char *port, std::vector<std::unique_ptr<dgalNode> >& nodes){
 	struct addrinfo *info, hints;
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_INET6;
@@ -17,12 +33,12 @@ bool listen(char *port){
 	int listenSocket = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
 	if(listenSocket == -1){
 		std::cerr << "Error listening: " << errno << " " << strerror(errno) << std::endl;
-		return false;
+		return;
 	}
 
 	if(bind(listenSocket, info->ai_addr, info->ai_addrlen) != 0){
 		std::cerr << "Error binding to port " << port << std::endl;
-		return false;
+		return;
 	}
 
 	if(listen(listenSocket, 10) == 0){
@@ -34,22 +50,36 @@ bool listen(char *port){
 		setsockopt(listenSocket, SOL_SOCKET, IPV6_V6ONLY, &optval, sizeof(optval));
 
 		while(true){
-			
+			//Accept new connections and add them to the nodes list
 		}
 	}else{
 		std::cerr << "Error listening " << errno << " " << strerror(errno) << std::endl;
-		return false;
+		return;
 	}
 
-	return true;
 }
 
-void heartBeatCheck(){
-
+void heartBeatCheck(const std::vector<std::unique_ptr<dgalNode> >& nodes){
+	while(true){
+		for(size_t i = 0; i < nodes.size(); i++){
+			//Check if alive
+		}
+	}
 }
 
 int main(int argc, char *argv[]){
-	listen("25665");
+	std::string port;
+	if(argc > 1){
+		port = argv[1];
+	}else{
+		port = "25665";
+	}
+
+	std::vector<std::unique_ptr<dgalNode> > nodes;
+
+	std::thread heartBeat(heartBeatCheck, std::ref(nodes));
+	handleNewConnections(port.c_str(), nodes);
+	heartBeat.join();
 
 	return 0;
 }
