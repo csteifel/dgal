a61e14483185fb2dcfeb9d5ecebab6d1fb3f29b7
Colin Steifel <colin@steifel.net>
2014-02-27 16:13:12 -0500

    Created thread worker pool creation for handling incoming messages, changed node list to use shared_ptr instead of unique_ptr to avoid accidentally deleting memory that could be in use.

--- coordinator/src/main.cpp
+++ coordinator/src/main.cpp
@@ -11,6 +11,9 @@
 #include <unistd.h>
 #include <chrono>
 #include <poll.h>
+#include <mutex>
+#include <queue>
+#include <unordered_set>
 #include <stdint.h>
 
 #include "main.h"
@@ -80,9 +83,69 @@ bool setUpListening(int &listenSocket, const char* port){
 	return true;
 }
 
-void socketWatch(const int listenSocket, std::vector<std::unique_ptr<dgalNode> >& nodes){
-	//TODO: Implement ability to watch sockets using poll
+void processMessages(std::queue<int>& socketsToProcess, std::queue<std::shared_ptr<dgalNode> >& nodesToProcess, std::unordered_set<int>& queuedSockets, std::mutex& processingLock){
+	while(true){
+		if(socketsToProcess.size() != 0){
+			processingLock.lock();
+			int fd = socketsToProcess.front();
+			socketsToProcess.pop();
+			std::shared_ptr<dgalNode> node = nodesToProcess.front();
+			nodesToProcess.pop();
+			processingLock.unlock();
 
+			//Create buffer to read in message type as well as size information
+			char buf[sizeof(uint8_t) + sizeof(uint64_t)];
+			memset(buf, 0, sizeof(buf));
+			int res = recv(fd, buf, sizeof(buf), MSG_DONTWAIT);
+
+			if(res > 0){
+				uint8_t type = (uint8_t) buf[0]; //No endian worry here only 1 byte
+
+
+				uint64_t convert;
+				memcpy(&convert, &buf[1], sizeof(uint64_t));
+
+				uint64_t length = ntohll(convert); //Convert to host endianness
+
+				if(type == 1){
+					//Heart beat recieved
+					node->recievedHeartBeat();
+					std::cout << "Recieved heart beat" << std::endl;
+				}else if(type == 2){
+					//Recieving serialized individuals
+					std::cout << "Recieving population from " << fd << std::endl;
+
+					//TODO: Add some safety barriers around allocating huge space
+					std::string message(length, 0);
+					res = recv(fd, &message[0], length, MSG_DONTWAIT);
+
+					if(res > 0 && (uint64_t) res == length){
+						std::cout << message << std::endl;
+					}else{
+						std::cerr << "Error reading message from socket! Supplied size wrong!" << std::endl;
+					}
+
+				}else{
+					//Stream error terminate connection
+					std::cerr << "Recieved non conforming message shutting down socket" << std::endl;
+					shutdown(fd, SHUT_RDWR);
+				}
+			}else if(res == -1){
+				//Error reading
+				std::cerr << "Error reading socket " << fd << ": " << errno << " " << strerror(errno) << std::endl;
+			}else{
+				//Disconnect
+				shutdown(fd, SHUT_RDWR);
+			}
+
+			processingLock.lock();
+			queuedSockets.erase(fd);
+			processingLock.unlock();
+		}
+	}
+}
+
+void socketWatch(const int listenSocket, std::vector<std::shared_ptr<dgalNode> >& nodes){
 	std::vector<struct pollfd> socketsToPoll;
 	struct pollfd listeningPollFD;
 
@@ -93,17 +156,40 @@ void socketWatch(const int listenSocket, std::vector<std::unique_ptr<dgalNode> >
 
 	socketsToPoll.push_back(listeningPollFD);
 
+	//Mutex for producer/consumers to use, should change to lock free implementation when time allows
+	std::mutex processingLock;
+
+	std::queue<int> socketsToProcess;
+	std::queue<std::shared_ptr<dgalNode> > nodesToProcess;
+	std::unordered_set<int> queuedSockets;
+
+	size_t numThreads = std::thread::hardware_concurrency();
+	if(numThreads == 0){
+		std::cerr << "Could not determine number of threads to use. Defaulting to 2" << std::endl;
+		numThreads = 1;
+	}else{
+		//Take one off to account for this thread
+		numThreads--;
+	}
+
+	std::vector<std::thread> processingThreads;
+
+	for(size_t i = 0; i < numThreads; i++){
+		processingThreads.push_back(std::thread(processMessages, std::ref(socketsToProcess), std::ref(nodesToProcess), std::ref(queuedSockets), std::ref(processingLock)));
+	}
+
 	while(true){
 		//Wait a maximum of 30 seconds if no response than we either have no nodes or they all disconnected
-		int res = poll(socketsToPoll.data(), socketsToPoll.size(), 30000);
+		int res = poll(socketsToPoll.data(), socketsToPoll.size(), 3000);
 
 		if(res > 0){
 			for(unsigned int i = 0; i < socketsToPoll.size(); i++){
 				if(socketsToPoll[i].revents == 0){
 					continue;
 				}
+
+
 				if(socketsToPoll[i].fd == listenSocket){
-					std::cout << "Accepting connection" << std::endl;
 					//We are accepting new connections
 					struct pollfd newPollWatch;
 					newPollWatch.fd = handleNewConnections(listenSocket, nodes);
@@ -113,46 +199,53 @@ void socketWatch(const int listenSocket, std::vector<std::unique_ptr<dgalNode> >
 				}
 
 
-				if(socketsToPoll[i].revents & POLLHUP || socketsToPoll[i].revents & POLLRDHUP){
-					std::cout << "POLLHUP" << std::endl;
+				bool foundNode = false;
+				size_t foundAt;
+				for(foundAt = 0; foundAt < nodes.size(); foundAt++){
+					if(socketsToPoll[i].fd == nodes[foundAt]->getSockFD()){
+						foundNode = true;
+						break;
+					}
 				}
 
-
-				switch(socketsToPoll[i].revents){
-					case POLLERR:
-						std::cout << "POLLERR" << std::endl;
-						break;
-					case POLLRDHUP:
-						std::cout << "POLLRDHUP" << std::endl;
-						break;
-					case POLLHUP:
-						std::cout << "POLLHUP" << std::endl;
-						break;
-					case POLLIN:
-						char a[10];
-						memset(a, 0, 10);
-						if(recv(socketsToPoll[i].fd, a, 10, 0)){
-							std::cout << a << std::endl;
-						}
-						break;
-					case POLLPRI:
-						std::cout << "POLLPRI" << std::endl;
-						break;
-					default:
-						std::cout << "DEFAULT" << std::endl;
-						exit(1);
+				if(foundNode == false){
+					std::cerr << "Cannot find node for socket " << socketsToPoll[i].fd << std::endl;
+					shutdown(socketsToPoll[i].fd, SHUT_RDWR);
+					close(socketsToPoll[i].fd);
+					socketsToPoll.erase(socketsToPoll.begin()+i);
+					continue;
 				}
+
+				if(socketsToPoll[i].revents & POLLERR){
+					std::cerr << "POLLERR " << errno << " " << strerror(errno) << std::endl;
+				}else if(socketsToPoll[i].revents & POLLHUP || socketsToPoll[i].revents & POLLRDHUP){
+					close(socketsToPoll[i].fd);
+					nodes.erase(nodes.begin() + foundAt);
+					socketsToPoll.erase(socketsToPoll.begin() + i);
+				}else if(socketsToPoll[i].revents & POLLIN){
+					if(queuedSockets.count(socketsToPoll[i].fd) == 1){
+						//we already have processing lined up for this node
+						continue;
+					}
+					processingLock.lock();
+					socketsToProcess.push(socketsToPoll[i].fd);
+					nodesToProcess.push(nodes[foundAt]);
+					queuedSockets.insert(socketsToPoll[i].fd);
+					processingLock.unlock();
+				}
+
 
 			}
-		}else{
-			//TODO: Go through and mark all nodes as dead if any exist
 		}
 	}
 
+	for(size_t i = 0; i < processingThreads.size(); i++){
+		processingThreads[i].join();
+	}
 }
 
 
-int handleNewConnections(const int listenSocket, std::vector<std::unique_ptr<dgalNode> >& nodes){
+int handleNewConnections(const int listenSocket, std::vector<std::shared_ptr<dgalNode> >& nodes){
 	struct sockaddr_storage clientInfo;
 	int clientSocket;
 	socklen_t sockStoreSize = sizeof(struct sockaddr_storage);
@@ -164,9 +257,8 @@ int handleNewConnections(const int listenSocket, std::vector<std::unique_ptr<dga
 		return -1;
 	}
 
-
-	std::unique_ptr<dgalNode> newClientNode(new dgalNode(clientSocket, "test", 2));
-	nodes.push_back(std::move(newClientNode));
+	std::shared_ptr<dgalNode> newClientNode(new dgalNode(clientSocket, "test", 2));
+	nodes.push_back(newClientNode);
 
 	return clientSocket;
 }
@@ -193,7 +285,7 @@ int main(int argc, char *argv[]){
 	}
 
 	int listeningSocket;
-	std::vector<std::unique_ptr<dgalNode> > nodes;
+	std::vector<std::shared_ptr<dgalNode> > nodes;
 
 	if(setUpListening(listeningSocket, port.c_str()) == false){
 		return 1;
--- coordinator/src/main.h
+++ coordinator/src/main.h
@@ -3,11 +3,13 @@
 
 #include <memory>
 #include <vector>
+#include <mutex>
+#include <queue>
+#include <unordered_set>
 #include "dgalnode.h"
 
 bool setUpListening(int&, const char*);
-int handleNewConnections(const int, std::vector<std::unique_ptr<dgalNode> >&);
+int handleNewConnections(const int, std::vector<std::shared_ptr<dgalNode> >&);
 void socketWatch(const int);
-
-
+void processMessages(std::queue<int>&, std::queue<std::shared_ptr<dgalNode> >&, std::unordered_set<int>&, std::mutex&);
 #endif
